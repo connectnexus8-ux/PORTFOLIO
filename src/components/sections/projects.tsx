601c2c3aae42c7455bc00c7f510ec98c30923559
Drashti <connectnexus8@gmail.com>
2026-02-20 15:27:53 +0530

    d

--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -43,7 +43,7 @@ const Projects = () => {
             ) : (
                 <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
                 {projects.map((project, index) => (
-                    <div key={index} className="h-[400px]">
+                    <div key={index} className="h-full">
                     <ProjectCard project={project} />
                     </div>
                 ))}
